3e8260345247ce9f514edb56d47b3d2023b0cee7
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2025-08-05 00:11:02 +0700

    fix: исправил хелпер

--- src/scenes/movieSearch.scene.ts
+++ src/scenes/movieSearch.scene.ts
@@ -13,91 +13,91 @@ import { MoviePaginationCallback } from "../callbacks/moviePagination.callback";
 import { CancelKeyboard } from "../buttons/keyboard/cancel.keyboard";
 import { CANCEL_TEXT, LEAVE_SCENE_TEXT } from "../constants/text.constants";
 import {
-  MOVIE_END_DATA,
-  MOVIE_NEXT_DATA,
-  MOVIE_PREV_DATA,
-  MOVIE_START_DATA,
+	MOVIE_END_DATA,
+	MOVIE_NEXT_DATA,
+	MOVIE_PREV_DATA,
+	MOVIE_START_DATA,
 } from "../constants/callbackData.constants";
 
 export class MovieSearchScene extends WizardScene {
-  constructor(public bot: Telegraf<IBotContext>) {
-    super(bot);
-  }
+	constructor(public bot: Telegraf<IBotContext>) {
+		super(bot);
+	}
 
-  getScene() {
-    return new Scenes.WizardScene<IBotContext>(
-      MOVIE_SEARCH_SCENE,
-      (ctx: IBotContext) => {
-        this.start(ctx);
-      },
-      (ctx: IBotContext) => {
-        this.sendMovies(ctx);
-      }
-    );
-  }
+	getScene() {
+		return new Scenes.WizardScene<IBotContext>(
+			MOVIE_SEARCH_SCENE,
+			(ctx: IBotContext) => {
+				this.start(ctx);
+			},
+			(ctx: IBotContext) => {
+				this.sendMovies(ctx);
+			}
+		);
+	}
 
-  protected async start(ctx: IBotContext) {
-    try {
-      await ctx.replyWithHTML(ENTER_MOVIE_TITLE, CancelKeyboard.getKeyboard());
-      return await ctx.wizard.next();
-    } catch (error: unknown) {
-      new ErrorHelper().sendWizardSceneError(ctx, error);
-    }
-  }
+	protected async start(ctx: IBotContext) {
+		try {
+			await ctx.replyWithHTML(ENTER_MOVIE_TITLE, CancelKeyboard.getKeyboard());
+			return await ctx.wizard.next();
+		} catch (error: unknown) {
+			new ErrorHelper().sendWizardSceneError(ctx, error);
+		}
+	}
 
-  private async sendMovies(ctx: IBotContext) {
-    try {
-      if (ctx.text === CANCEL_TEXT) {
-        await ctx.replyWithHTML(LEAVE_SCENE_TEXT, Markup.removeKeyboard());
-        return await ctx.scene.leave();
-      }
+	private async sendMovies(ctx: IBotContext) {
+		try {
+			if (ctx.text === CANCEL_TEXT) {
+				await ctx.replyWithHTML(LEAVE_SCENE_TEXT, Markup.removeKeyboard());
+				return await ctx.scene.leave();
+			}
 
-      const query = ctx.text!;
-      const { docs, page, pages } =
-        await AsyncMessage.sendWithProgress<IMovieSearchResponse>(
-          () => APIMovies.search(query),
-          ctx
-        );
-      const movie = docs[0];
-      const messageId = ctx.message?.message_id!;
-      const startCallbackData = `${MOVIE_START_DATA}-${messageId}`;
-      const prevCallbackData = `${MOVIE_PREV_DATA}-${messageId}`;
-      const nextCallbackData = `${MOVIE_NEXT_DATA}-${messageId}`;
-      const endCallbackData = `${MOVIE_END_DATA}-${messageId}`;
+			const query = ctx.text!;
+			const { docs, page, pages } =
+				await AsyncMessage.sendWithProgress<IMovieSearchResponse>(
+					() => APIMovies.search(query),
+					ctx
+				);
+			const movie = docs[0];
+			const messageId = ctx.message?.message_id;
+			const startCallbackData = `${MOVIE_START_DATA}-${messageId}`;
+			const prevCallbackData = `${MOVIE_PREV_DATA}-${messageId}`;
+			const nextCallbackData = `${MOVIE_NEXT_DATA}-${messageId}`;
+			const endCallbackData = `${MOVIE_END_DATA}-${messageId}`;
 
-      if (movie) {
-        const movieService = new MoviesService(movie, page, pages);
+			if (movie) {
+				const movieService = new MoviesService(movie, page, pages);
 
-        await ctx.replyWithPhoto(movieService.getPhoto(), {
-          caption: movieService.getPhotoCaption(),
-          parse_mode: "HTML",
-          reply_markup: new PaginationButtons(
-            startCallbackData,
-            prevCallbackData,
-            nextCallbackData,
-            endCallbackData
-          ).getButtons(page, pages).reply_markup,
-        });
+				await ctx.replyWithPhoto(movieService.getPhoto(), {
+					caption: movieService.getPhotoCaption(),
+					parse_mode: "HTML",
+					reply_markup: new PaginationButtons(
+						startCallbackData,
+						prevCallbackData,
+						nextCallbackData,
+						endCallbackData
+					).getButtons(page, pages).reply_markup,
+				});
 
-        new MoviePaginationCallback(this.bot, {
-          startCallbackData,
-          prevCallbackData,
-          nextCallbackData,
-          endCallbackData,
-          query,
-          currentPage: page,
-          totalPages: pages,
-        }).init();
-      } else {
-        await ctx.replyWithHTML(
-          `Фильм '${query}' не найден`,
-          Markup.removeKeyboard()
-        );
-      }
+				new MoviePaginationCallback(this.bot, {
+					startCallbackData,
+					prevCallbackData,
+					nextCallbackData,
+					endCallbackData,
+					query,
+					currentPage: page,
+					totalPages: pages,
+				}).init();
+			} else {
+				await ctx.replyWithHTML(
+					`Фильм '${query}' не найден`,
+					Markup.removeKeyboard()
+				);
+			}
 
-      return await ctx.scene.leave();
-    } catch (error: unknown) {
-      new ErrorHelper().sendWizardSceneError(ctx, error);
-    }
-  }
+			return await ctx.scene.leave();
+		} catch (error: unknown) {
+			new ErrorHelper().sendWizardSceneError(ctx, error);
+		}
+	}
 }
